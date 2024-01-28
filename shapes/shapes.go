e63c6a599cf76ba6cf00b421ee4de46a523613e8
angusfng <angusfeng99@gmail.com>
2024-01-28 17:54:58 +1100

    finish shapes

--- shapes/shapes.go
+++ shapes/shapes.go
@@ -1,18 +1,30 @@
 package shapes
 
+import "math"
+
+// As long as the struct matches the interface, it implements
+type Shape interface {
+	Area() float64
+}
+
 type Rectangle struct {
 	Width  float64
 	Height float64
 }
-
 type Circle struct {
 	Radius float64
 }
-
-func Perimeter(rectangle Rectangle) float64 {
-	return 2 * (rectangle.Height * rectangle.Width)
+type Triangle struct {
+	Base   float64
+	Height float64
 }
 
-func Area(rectangle Rectangle) float64 {
-	return rectangle.Height * rectangle.Width
+func (r Rectangle) Area() float64 {
+	return r.Width * r.Height
+}
+func (c Circle) Area() float64 {
+	return math.Pi * math.Pow(c.Radius, 2)
+}
+func (t Triangle) Area() float64 {
+	return 0.5 * t.Base * t.Height
 }
--- shapes/shapes_test.go
+++ shapes/shapes_test.go
@@ -2,34 +2,30 @@ package shapes
 
 import "testing"
 
-func TestPerimeter(t *testing.T) {
-	rectangle := Rectangle{10.0, 10.0}
-	got := Perimeter(rectangle)
-	want := 40.0
-
-	if got != want {
-		t.Errorf("got %.2f want %.2f", got, want)
-	}
-}
-
 func TestArea(t *testing.T) {
-	t.Run("Rectangle", func(t *testing.T) {
-		rectangle := Rectangle{5.0, 6.0}
-		got := Area(rectangle)
-		want := 30.0
 
-		if got != want {
-			t.Errorf("got %.2f want %.2f", got, want)
+	checkArea := func(t testing.TB, shape Shape, hasArea float64) {
+		t.Helper()
+		got := shape.Area()
+
+		if got != hasArea {
+			t.Errorf("%#v got %g, hasArea %g", shape, got, hasArea)
 		}
-	})
+	}
 
-	t.Run("Circle", func(t *testing.T) {
-		circle := Circle{10}
-		got := Area(circle)
-		want := 314.1592653589793
+	areaTests := []struct {
+		name    string
+		shape   Shape
+		hasArea float64
+	}{
+		{name: "Rectangle", shape: Rectangle{5.0, 6.0}, hasArea: 30.0},
+		{name: "Circle", shape: Circle{10}, hasArea: 314.1592653589793},
+		{name: "Triangle", shape: Triangle{10.0, 5.0}, hasArea: 25.0},
+	}
 
-		if got != want {
-			t.Errorf("got %g want %g", got, want)
-		}
-	})
+	for _, test := range areaTests {
+		t.Run(test.name, func(t *testing.T) {
+			checkArea(t, test.shape, test.hasArea)
+		})
+	}
 }

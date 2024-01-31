f67ff9b3d967ef64ed907ed91bbfbfc9773a2b72
angusfng <angusfeng99@gmail.com>
2024-01-31 21:53:36 +1100

    wallet in progress

--- wallet/wallet.go
+++ wallet/wallet.go
@@ -12,17 +12,22 @@ type Wallet struct {
 	balance Bitcoin
 }
 
-func (w *Wallet) Deposit(amount Bitcoin) {
-	fmt.Printf("address of balance in Deposit is %p \n", &w.balance)
-	// Automatically dereferences
-	w.balance += amount
-} 
-
-func (w *Wallet) Balance() Bitcoin {
-	return w.balance
-}
-
 // Implements stringer on Bitcoin type so that %s will work with Bitcoin types
 func (b Bitcoin) String() string {
 	return fmt.Sprintf("%d BTC", b)
+}
+
+// Pointer to wallet because we want to modify it
+func (w *Wallet) Deposit(amount Bitcoin) {
+	// Automatically dereferences
+	w.balance += amount
+}
+
+// Passes in a copy
+func (w Wallet) Balance() Bitcoin {
+	return w.balance
+}
+
+func (w *Wallet) Withdraw(amount Bitcoin) {
+	w.balance -= amount
 }

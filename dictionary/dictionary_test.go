7c80fc9ba69ed0615290f318af776e19358cfcc7
Angus Feng <afeng@freelancer.com>
2024-02-05 17:35:28 +1100

    dictionary in progress

---
+++ dictionary/dictionary_test.go
@@ -0,0 +1,20 @@
+package dictionary
+
+import "testing"
+
+func TestSearch(t *testing.T) {
+	dictionary := map[string]string{"test": "test string"}
+	
+	got := Search(dictionary, "test")
+	want := "test string"
+
+	assertStrings(t, got, want)
+}
+
+func assertStrings(t testing.TB, got, want string) {
+	t.Helper()
+
+	if got != want {
+		t.Errorf("got %q, want %q, given %q", got, want, "test")
+	}
+}
